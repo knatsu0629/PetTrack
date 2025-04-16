b79edd2587de0516f848e91e9bb0067cab1ca163
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-04-16 10:22:03 +0000

    Create new post page and update create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new  
   end
 
   def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to post_path
+    else
+      render :new
+    end    
   end
 
   def edit
