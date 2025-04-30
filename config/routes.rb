ce7dcd5468a5db2bbef3f76c94bc6fe4556102cc
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-04-30 10:06:35 +0000

    Generate Searches controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,13 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    collection do
-      get 'search'
-    end
-  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/about', to: 'homes#about'
+  
+  get "search", to: "searches#search"
 
   resources :posts
   resources :lost_pets
