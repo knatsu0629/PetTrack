e0f15701f99928bffac6fa85d962b31e3796488b
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-04-30 10:15:04 +0000

    Implement search action in SearchesController

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,16 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search
+    @model = params[:model]
+    @keyword = params[:keyword]
+  
+    if @model == "user"
+      @users = User.where("name LIKE ?", "%#{@keyword}%")
+    elsif @model == "post"
+       @posts = Post.where("caption LIKE ?", "%#{@keyword}%")
+    else
+      flash[:alert] = "検索対象を選んでください"
+    end
+  end
 end
