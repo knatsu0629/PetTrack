dbb889c004fbeb8b9f9a2826406d78626342fb46
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-05-01 12:10:44 +0000

    Add '迷子ペット' option to search model select box

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,14 +4,26 @@ class SearchesController < ApplicationController
   def search
     @model = params[:model]
     @keyword = params[:keyword]
+    @method = params[:method] 
+  
+    if @method == "perfect"
+      search_word = @keyword
+    else
+      search_word = "%#{@keyword}%"
+    end
   
     if @model == "user"
-      @results = User.where("name LIKE ?", "%#{@keyword}%")
+      @users = User.where("name LIKE ?", search_word)
     elsif @model == "post"
-      @results = Post.where("caption LIKE ?", "%#{@keyword}%")
+      @posts = Post.where("title LIKE ? OR body LIKE ?", search_word, search_word)
+    elsif @model == "lost_pet"
+      @lost_pets = LostPet.where("name LIKE ? OR feature LIKE ? OR animal_type LIKE ? OR last_seen_location LIKE ?", search_word, search_word, search_word, search_word)
     else
       flash[:alert] = "検索対象を選んでください"
-      @results = []  
     end
+
+    @users ||= []
+    @posts ||= []
+    @lost_pets ||= []
   end
-end
+end 
