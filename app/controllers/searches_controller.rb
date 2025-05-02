c44c14945c8e3af34f7dcd82f27e5456da748925
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-05-02 09:46:55 +0000

    Add flash alert to search screen and redirect when keyword is empty

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,13 +5,19 @@ class SearchesController < ApplicationController
     @model = params[:model]
     @keyword = params[:keyword]
     @method = params[:method] 
-  
+
+    if @keyword.blank?
+      flash[:alert] = "検索ワードを入力してください"
+      redirect_to request.referer and return
+    end
+
+
     if @method == "perfect"
       search_word = @keyword
     else
       search_word = "%#{@keyword}%"
     end
-  
+
     if @model == "user"
       @users = User.where("name LIKE ?", search_word)
     elsif @model == "post"
@@ -20,10 +26,7 @@ class SearchesController < ApplicationController
       @lost_pets = LostPet.where("name LIKE ? OR feature LIKE ? OR animal_type LIKE ? OR last_seen_location LIKE ?", search_word, search_word, search_word, search_word)
     else
       flash[:alert] = "検索対象を選んでください"
+      redirect_to root_path
     end
-
-    @users ||= []
-    @posts ||= []
-    @lost_pets ||= []
   end
 end 
