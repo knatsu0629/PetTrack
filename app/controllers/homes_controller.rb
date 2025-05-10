8deefe2b3568b1e4b659db6067d21468252a403a
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-05-10 06:37:17 +0000

    Add admin dashboard and user management feature

--- app/controllers/homes_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:top] 
-  
-    def top
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,36 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'public/users/sessions'
   }
 
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/guest_sign_in', to: 'public/users/sessions#guest_sign_in'
   end
 
-  resources :posts do
-    resources :comments, only: [:create, :destroy]
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about', to: 'homes#about'
+
+    get "search", to: "searches#search"
+
+    resources :posts do
+      resources :comments, only: [:create, :destroy]
+    end
+
+    resources :lost_pets do
+      resources :lost_pet_comments, only: [:index, :create, :destroy]
+    end
+
+    resources :users, only: [:show, :edit, :update, :destroy]
   end
 
-  resources :lost_pets do
-    resources :lost_pet_comments, only: [:create, :destroy]
+  namespace :admin do
+    root to: 'dashboards#index'
+    resources :dashboards, only: [:index] 
+    resources :users, only: [:index, :show, :destroy]
   end
-  
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
-  get '/about', to: 'homes#about'
-  
-  get "search", to: "searches#search"
-
-  resources :posts
-  resources :lost_pets
-  resources :users, only: [:show, :edit, :update, :destroy]
-
 end
-
