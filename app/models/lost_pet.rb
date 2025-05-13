06dafb9907b6d612b6149b943422b3085348bc5f
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-05-13 09:25:24 +0000

    Fix gender display to show in Japanese (オス, メス, 不明)

--- app/models/lost_pet.rb
+++ app/models/lost_pet.rb
@@ -1,10 +1,18 @@
 class LostPet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  enum gender: { male: 0, female: 1, unknown: 2 }
-
   has_many :lost_pet_comments, dependent: :destroy
-
+  enum gender: { male: 0, female: 1, unknown: 2 }
+    def gender_label
+      case gender
+      when "male"
+        "オス"
+      when "female"
+        "メス"
+      when "unknown"
+        "不明"
+      end
+    end
 end
 
 
