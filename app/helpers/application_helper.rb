ab8bc7f34382591c0b68045356247748bf398def
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-06-15 05:31:59 +0000

    feat: Add 'just now' display for recent posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def custom_posted_time(timestamp)
+    # 投稿されてから60秒未満の場合
+    if (Time.current - timestamp) < 60
+      "たった今投稿"
+    else
+      # 60秒以上の場合
+      time_ago_raw = time_ago_in_words(timestamp)
+      formatted_time = time_ago_raw.strip
+                                    .sub('about', '')
+                                    .gsub('minutes', '分')
+                                    .gsub('minute', '分')
+                                    .gsub('hours', '時間')
+                                    .gsub('hour', '時間')
+                                    .gsub('days', '日')
+                                    .gsub('day', '日')
+      "#{formatted_time}前に投稿"
+    end
+  end
 end
